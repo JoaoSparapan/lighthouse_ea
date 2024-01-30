7b8346873d5ad746949054a1844e76d390449004
João Marcelo Sparapan Servilha Oliveira <sparapan15@hotmail.com>
2024-01-30 11:49:22 +0000

    refactor da dim colaboradores

--- models/marts/dim_colaboradores.sql
+++ models/marts/dim_colaboradores.sql
@@ -6,4 +6,19 @@ with
         from {{ ref('stg_erp__colaboradores') }}
     )
 
-select * from stg_colaboradores
+    , stg_colaborador_agencia as (
+        select
+            *
+        from {{ ref('stg_erp__colaborador_agencia') }}
+    )
+
+    , joined_table as (
+        select
+            stg_colaboradores.id_colaborador
+            , stg_colaborador_agencia.id_agencia
+            , stg_colaboradores.nome_completo_colaborador
+        from stg_colaboradores
+        left join stg_colaborador_agencia on stg_colaboradores.id_colaborador = stg_colaborador_agencia.id_colaborador
+    )
+
+select * from joined_table
---
+++ models/staging/stg_erp__colaborador_agencia.sql
@@ -0,0 +1,9 @@
+with
+    fonte_colaborador_agencia as (
+        select
+            cast(cod_colaborador as int) as id_colaborador
+            , cast(cod_agencia as int) as id_agencia
+        from {{ source('erp', 'colaborador_agencia') }}
+    )
+
+select * from fonte_colaborador_agencia
